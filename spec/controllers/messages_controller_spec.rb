4e1bcd62a076e3469b4534f17fd0d8861d3fb041
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-12-03 06:44:07 +0900

    add new test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -12,4 +12,14 @@ describe MessagesController do
       expect(response).to render_template :index
     end
   end
+  describe 'GET #new' do
+    it 'can get Message instance' do
+      get :new
+      expect(assigns(:message)).to be_a_new(Message)
+    end
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
 end
