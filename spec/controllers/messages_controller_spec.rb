beeecbb0950ec8a404488eecd05a8b81fd34b584
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-12-03 07:02:29 +0900

    add create test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -22,4 +22,28 @@ describe MessagesController do
       expect(response).to render_template :new
     end
   end
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'save the new message in the database' do
+        expect{
+          post :create, message: FactoryGirl.attributes_for(:message)
+        }.to change(Message, :count).by(1)
+      end
+      it 'redirects to messages#show' do
+        post :create, message: FactoryGirl.attributes_for(:message)
+        expect(response).to redirect_to message_path(assigns[:message])
+      end
+    end
+    context 'with invalid attributes' do
+      it 'does not save the new message in the database' do
+        expect{
+          post :create, message: FactoryGirl.attributes_for(:message, title: '')
+        }.to_not change(Message, :count)
+      end
+      it 're-renders the :new tempalte' do
+        post :create, message: FactoryGirl.attributes_for(:message, title:'')
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
