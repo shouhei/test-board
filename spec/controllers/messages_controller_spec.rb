e2554e713ded9d9d5dbbb7b055cc25c40eadcbff
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-12-03 06:41:13 +0900

    add get test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,7 +2,14 @@ require 'spec_helper'
 
 describe MessagesController do
   describe 'GET #index' do
-    it 'can get all message'
-    it 'renders the index template'
+    it 'can get all message' do
+      message = FactoryGirl.create(:message)
+      get :index
+      expect(assigns(:messages)).to match_array([message])
+    end
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template :index
+    end
   end
 end
